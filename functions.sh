4f24c2e12d06e3bc9d4d68a2019a07ba067af1c3
pkgdemon <pkgdemonteam@gmail.com>
2025-06-15 13:13:09 -0500

    Disable pkg fetch

--- functions.sh
+++ functions.sh
@@ -22,7 +22,7 @@ create_datasets() {
   base="zroot/gnustep-build"
 
   for ds in "$base" "$base/etc" "$base/distfiles"; do
-    mount="/$ds"  # This expands to /zroot/gnustep-build/etc etc.
+    mount="/$ds"
 
     if ! zfs list -H -o name "$ds" >/dev/null 2>&1; then
       zfs create -o mountpoint="$mount" "$ds"
@@ -36,7 +36,7 @@ install_poudriere_conf() {
 
 poudriere_jail() {
   jailname="gnustep_base"
-  jailversion="14.2-RELEASE"  # Match what you're building from /usr/src
+  jailversion="14.2-RELEASE"
 
   if ! poudriere -e "$POUDRIERE_ETC" jail -l | grep -q "^$jailname"; then
     poudriere -e "$POUDRIERE_ETC" jail -c \
@@ -61,7 +61,7 @@ poudriere_overlay() {
 }
 
 poudriere_bulk() {
-  poudriere -e "$POUDRIERE_ETC" bulk -b latest -j gnustep_base -p gnustep_ports -O gnustep_overlay $(cat ports.list)
+  poudriere -e "$POUDRIERE_ETC" bulk -j gnustep_base -p gnustep_ports -O gnustep_overlay $(cat ports.list)
 }
 
 ports_target() {
